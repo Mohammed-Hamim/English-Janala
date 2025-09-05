0042e2bd3ed6334a0f4a4cae89e4e90e1e8bb8e9
Mohammed- Hamim <mohammedhamim0158@gmail.com>
2025-09-06 02:52:02 +0600

    added word details and handled error

--- script.js/index.js
+++ script.js/index.js
@@ -29,14 +29,14 @@ const displayLessons = (lessons) => {
 // function for loading word
 const loadWordLevel = (id) => {
     const wordUrl = `https://openapi.programming-hero.com/api/level/${id}`
-    console.log(wordUrl)
+
     fetch(wordUrl)
         .then(response => response.json())
         .then(wordLevel => {
-            removeActive(); // remove active class 
+            removeActive(); // remove all active class 
             // select lesson button 
             const clickBtn = document.getElementById(`lesson-btn-${id}`);
-            clickBtn.classList.add("active"); // added active btn color
+            clickBtn.classList.add("active"); // add active class
             displayWords(wordLevel.data);
         })
 }
@@ -61,8 +61,8 @@ const displayWords = (words) => {
         wordContainer.innerHTML = `
          <div class="flex flex-col items-center justify-between col-span-full space-y-3">
                 <img src="./assets/alert-error.png" alt="">
-                <p class="text-xl text-[#79716B] font-bangla">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
-                <h2 class="text-4xl text-[#292524] font-bold font-bangla">নেক্সট Lesson এ যান</h2>               
+                <p class="md:text-xl text-[#79716B] font-bangla">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                <h2 class="md:text-4xl text-xl text-[#292524] font-bold font-bangla">নেক্সট Lesson এ যান</h2>               
             </div>        
          `;
         return
@@ -75,11 +75,14 @@ const displayWords = (words) => {
         wordCard.innerHTML = `
          
              <div class="bg-white text-center space-y-5 p-6 rounded-xl">
+             
+            <!--conditional rendering {${word.word ? word.word : "Word Not Found"}}-->
+
                 <h2 class="text-4xl font-bold">${word.word ? word.word : "Word Not Found"}</h2>
                 <p class="font-semibold ">Meaning / Pronunciation</p>
                 <p class= "font-bangla font-bold text-lg">${word.meaning ? word.meaning : "Meaning Not Found"} / ${word.pronunciation ? word.pronunciation : "Pronunciation Not Found"}</p>
                 <div class="flex justify-between">
-                    <button class="btn hover:bg-[#1A91FF80] bg-[#1A91FF10]"><i class="fa-solid fa-circle-info"></i>
+                    <button onclick= "loadWordDetails(${word.id})" class="btn hover:bg-[#1A91FF80] bg-[#1A91FF10]"><i class="fa-solid fa-circle-info"></i>
                     </button>
                     <button class="btn hover:bg-[#1A91FF80] bg-[#1A91FF10]"><i class="fa-solid fa-volume-high"></i>
                     </button>
@@ -93,4 +96,50 @@ const displayWords = (words) => {
     });
 
 }
+
+
+// function for loading words details
+const loadWordDetails = async (id) => {
+
+    const wordDetails = `https://openapi.programming-hero.com/api/word/${id}`;
+    const res = await fetch(wordDetails);
+    const details = await res.json();
+    displayWordDetails(details.data);
+}
+
+
+// function for displaying words details
+const displayWordDetails = (data) => {
+    const detailContainer = document.getElementById("detail-container");
+    detailContainer.innerHTML = `
+                 <h3 class="font-bold text-xl">${data.word}<i class="fa-solid fa-microphone-lines"></i> :${data.pronunciation}</h3>
+                     <div>
+                        <h2 class="font-bold text-xl ">Meaning</h2>
+                        <p>${data.meaning}</p>
+                     </div>
+                     <div>
+                        <h2 class="font-bold text-xl ">Example</h2>
+                        <p>${data.sentence}</p>
+                     </div>
+                     <div>
+                        <h2 class="font-bold text-xl ">সমার্থক শব্দ গুলো</h2>
+                        <div>${createElements(data.synonyms)}</div>
+
+                     </div>
+                    <div class="modal-action">
+                        <form method="dialog">
+                            <!-- if there is a button in form, it will close the modal -->
+                            <button class="btn">Close</button>
+                        </form>
+                    </div>
+    ` ;
+    document.getElementById("my_modal_5").showModal();
+}
+
+// function for creating span element for showing synonyms
+const createElements = (arr) => {
+    const htmlElements = arr.map(element => `<span class="btn">${element}</span>`);
+    return htmlElements.join(" ");
+}
+
 loadLessons()
